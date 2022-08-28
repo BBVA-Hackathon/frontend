870a35d6dc46de06b7e8cb7678125dab8f22679c
Marco Atencio <marco_atencio@hotmail.com>
2022-08-28 15:46:06 -0500

    sad

--- src/components/card/style.js
+++ src/components/card/style.js
@@ -42,8 +42,8 @@ export const StatusContent = styled.div`
       : props.percent > 0.4
       ? '#00c853'
       : props.percent > 0.8
-      ? 'white'
-      : 'white'};
+      ? 'yellow'
+      : 'yellow'};
   display: flex;
   justify-content: center;
   align-items: center;
