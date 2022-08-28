5202e8bca2ede3839dc474c8e5ea1b75ddc6e55b
Marco Atencio <marco_atencio@hotmail.com>
2022-08-28 08:53:41 -0500

    new style list map

--- src/pages/ListMap/index.jsx
+++ src/pages/ListMap/index.jsx
@@ -1,11 +1,39 @@
-import { MapContainer, ListMapContainer } from './style';
+import {
+  MapContainer,
+  ListMapContainer,
+  ItemListStyle,
+  NameBank,
+  DirectionBank,
+  DetailBank,
+  StatusBank,
+  ScheduleBank,
+} from './style';
 
 const ListMap = () => {
   return (
     <MapContainer>
-      <ListMapContainer>Lista de Bancos</ListMapContainer>
+      <ListMapContainer>
+        <ItemListMap />
+        <ItemListMap />
+      </ListMapContainer>
       <div> map</div>
     </MapContainer>
   );
 };
+
+const ItemListMap = () => {
+  return (
+    <ItemListStyle>
+      <NameBank>BBVA San Isidro</NameBank>
+      <DirectionBank>
+        Av. República de Panamá 3055, San Isidro 15036
+      </DirectionBank>
+      <DetailBank>
+        <StatusBank hour={2}>Cerrado</StatusBank>
+        <ScheduleBank> Abre a las 09:00 del lunes a viernes</ScheduleBank>
+      </DetailBank>
+    </ItemListStyle>
+  );
+};
+
 export default ListMap;
--- src/pages/ListMap/style.js
+++ src/pages/ListMap/style.js
@@ -6,11 +6,46 @@ export const MapContainer = styled.div`
 `;
 
 export const ListMapContainer = styled.div`
-  width: 400px;
+  width: 340px;
   justify-content: space-between;
   align-items: center;
   padding: 10px;
   cursor: pointer;
-  background-color: red;
   height: 700px;
+`;
+
+export const ItemListStyle = styled.div`
+  margin-left: 0px;
+  cursor: pointer;
+  border-bottom: 1px solid #333;
+  padding-bottom: 25px;
+  margin-bottom: 40px;
+`;
+
+export const NameBank = styled.h3`
+  font-size: 25px;
+  font-weight: bold;
+  color: #222222;
+  margin-bottom: 10px;
+`;
+
+export const DirectionBank = styled.p`
+  font-size: 14px;
+  color: #666666;
+  margin-bottom: 5px;
+`;
+
+export const DetailBank = styled.div`
+  display: flex;
+  margin-top: 10px;
+`;
+
+export const StatusBank = styled.p`
+  margin-right: 4px;
+  color: ${(props) => (props.hour > 9 ? '#00a65a' : '#f56954')};
+`;
+
+export const ScheduleBank = styled.p`
+  margin-left: 4px;
+  color: #444444;
 `;
